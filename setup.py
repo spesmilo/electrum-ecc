c50050c41235cf6ac487728f5bb0ea09065139e9
SomberNight <somber.night@protonmail.com>
2024-10-03 16:40:00 +0000

    build: add setuptools-based way to create binary wheels
    This allows linux and mac users to have "pip install" compile libsecp for them.
    
    Notes:
    - currently sdist bundles the libsecp sources. This potentially allows offline pip installing from sdist. The alternative would be to download this via http at build time, which is what e.g. coincurve does.
      - I think this is fine, but we should at least exclude some files from the sdist (atm we include the full git submodule).
    - what we have, the libsecp.so, is *not* a C extension!
      (as we don't link against the cpython ABI, but just have a generic C library that we interface with using ctypes)
      e.g. setuptools.Extension not for us.
        - see https://stackoverflow.com/questions/19701208/setuptools-how-to-build-shared-library-before-package-installation#comment129085776_21005058
    - TODO add option to skip compilation, e.g. using env var
      (or to ignore failure)
    
    Random refs:
    - https://setuptools.pypa.io/en/latest/userguide/ext_modules.html#pyproject-toml
    - https://stackoverflow.com/questions/42585210/extending-setuptools-extension-to-use-cmake-in-setup-py
    - https://stackoverflow.com/questions/76882974/packaged-python-c-extension-is-missing-pyinit-module-in-so-file
    - https://stackoverflow.com/questions/33168482/compiling-installing-c-executable-using-pythons-setuptools-setup-py
    - https://github.com/vpelletier/python-libusb1/blob/a03f4c3171d55a8325accc6b79246c235f0a6ce5/setup.sh

---
+++ setup.py
@@ -0,0 +1,123 @@
+# based on https://github.com/ludbb/secp256k1-py/blob/f5e455227bf1e833128adf80de8ee0ebcebf218c/setup.py
+
+import errno
+import logging
+import os
+import os.path
+import platform
+import shutil
+import subprocess
+
+from setuptools import setup
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+from wheel.bdist_wheel import safer_name, get_platform
+
+
+_logger = logging.getLogger("electrum_ecc")
+MAKE = 'gmake' if platform.system() in ['FreeBSD', 'OpenBSD'] else 'make'
+
+
+def absolute(*paths):
+    op = os.path
+    return op.realpath(op.abspath(op.join(op.dirname(__file__), *paths)))
+
+
+def compile_secp(build_dir: str) -> None:
+    if not os.path.exists(absolute('libsecp256k1')):
+        raise Exception("missing git submodule secp256k1")
+
+    if not os.path.exists(absolute('libsecp256k1/configure')):
+        # configure script hasn't been generated yet
+        autogen = absolute('libsecp256k1/autogen.sh')
+        os.chmod(absolute(autogen), 0o755)
+        subprocess.check_call([autogen], cwd=absolute('libsecp256k1'))
+
+    for filename in [
+        'libsecp256k1/configure',
+        'libsecp256k1/build-aux/compile',
+        'libsecp256k1/build-aux/config.guess',
+        'libsecp256k1/build-aux/config.sub',
+        'libsecp256k1/build-aux/depcomp',
+        'libsecp256k1/build-aux/install-sh',
+        'libsecp256k1/build-aux/missing',
+        'libsecp256k1/build-aux/test-driver',
+    ]:
+        try:
+            os.chmod(absolute(filename), 0o755)
+        except OSError as e:
+            # some of these files might not exist depending on autoconf version
+            if e.errno != errno.ENOENT:
+                # If the error isn't 'No such file or directory' something
+                # else is wrong and we want to know about it
+                raise
+
+    cmd = [
+        absolute('libsecp256k1/configure'),
+        '--enable-shared',
+        '--disable-static',
+        '--disable-dependency-tracking',
+        '--with-pic',
+        '--prefix',
+        os.path.abspath(build_dir),
+        '--enable-module-recovery',
+        '--enable-module-extrakeys',
+        '--enable-module-schnorrsig',
+        '--enable-experimental',
+        '--enable-module-ecdh',
+        '--enable-benchmark=no',
+        '--enable-tests=no',
+        '--enable-openssl-tests=no',
+        '--enable-exhaustive-tests=no',
+    ]
+
+    _logger.info('Running configure: {}'.format(' '.join(cmd)))
+    subprocess.check_call(cmd, cwd=build_dir)
+
+    subprocess.check_call([MAKE], cwd=build_dir)
+    subprocess.check_call([MAKE, 'install'], cwd=build_dir)
+
+
+class bdist_wheel(_bdist_wheel):
+
+    def finalize_options(self):
+        # Inject the platform name, e.g. "linux_x86_64".
+        # This will result in the final build artifact being named e.g.
+        #   electrum_ecc-0.0.2-py3-none-linux_x86_64.whl
+        self.plat_name = get_platform(self.bdist_dir)
+        # note: we don't set the python "impl tag" or the "abi tag", as the C lib we build
+        #       does not depend on them (it is not a "C extension" as we don't static link cpython).
+        _bdist_wheel.finalize_options(self)
+
+    def _build_and_copy_secp_lib(self):
+        _build_cmd = self.get_finalized_command('build')
+        build_dir = os.path.join(_build_cmd.build_base, "temp_libsecp")
+        target_dir = os.path.join(self.bdist_dir, safer_name(self.distribution.get_name()))
+        for dir_ in (build_dir, target_dir):
+            try:
+                os.makedirs(dir_)
+            except OSError as e:
+                if e.errno != errno.EEXIST:
+                    raise
+
+        compile_secp(build_dir)
+
+        build_temp_libs = os.path.join(build_dir, "lib")
+        for fname in os.listdir(build_temp_libs):
+            if (
+                    fname.endswith(".so") or fname.endswith(".dll") or fname.endswith(".dylib")
+                    or ".so." in fname  # FIXME symlink duplication
+            ):
+                srcpath = os.path.join(build_temp_libs, fname)
+                _logger.info(f"copying file {srcpath!r} to {target_dir=}")
+                shutil.copy2(srcpath, target_dir)
+
+    def run(self):
+        self._build_and_copy_secp_lib()
+        _bdist_wheel.run(self)
+
+
+setup(
+    cmdclass={
+        'bdist_wheel': bdist_wheel,
+    },
+)
